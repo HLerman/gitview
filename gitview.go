db31bea15d6b226189713499dfe806d62d936435
lerman <contact@lerman.fr>
2019-07-30 09:48:32 +0000

    v2 start

--- gitview.go
+++ gitview.go
@@ -3,11 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"os/user"
 	"path/filepath"
 	"sync"
 
@@ -19,68 +16,6 @@ var args struct {
 	Pull    bool `arg:"--pull" help:"Git pull on all repositories"`
 	Refresh bool `arg:"--refresh" help:"Create json file which contain the repositories path. This Json can be used to avoid searching phase"`
 }
-
-func getJSONPath() (string, error) {
-	usr, err := user.Current()
-
-	if err != nil {
-		return "", err
-	}
-
-	return usr.HomeDir + "/gitview.json", nil
-}
-
-func returnStringFromFile(path string) string {
-	dat, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(dat)
-}
-
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-func checkBinExists(bin string) {
-	if _, err := exec.LookPath(bin); err != nil {
-		log.Println("didn't find '" + bin + "' executable")
-		os.Exit(1)
-	}
-}
-
-func createJSONFile(repositories []string) error {
-	json, err := json.Marshal(repositories)
-
-	if err != nil {
-		return err
-	}
-
-	jsonPath, err := getJSONPath()
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(jsonPath, json, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-type Git struct {
-	branch string
-	diff   []string
-	status string
-}
-
-type Repository map[string]*Git
 
 func main() {
 	arg.MustParse(&args)
